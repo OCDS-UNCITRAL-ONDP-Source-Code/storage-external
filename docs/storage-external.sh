78cfcec31f478dc1f770b30b7b1db30e7ba90761
andreykolchev <andreykolchev@gmail.com>
2018-12-25 19:44:06 +0200

    first

--- docs/storage-external.sh
+++ docs/storage-external.sh
@@ -1,7 +1,7 @@
 java \
- -Xmx256m \
+ -Xmx512m \
  -Dfile.encoding=UTF-8 \
  -jar storage-external-1.1.1.jar \
  --spring.profiles.active.uri=default \
- --spring.cloud.config.uri=http://10.0.20.125:8700 \
- --spring.cloud.config.label=9e640b3
+ --spring.cloud.config.uri=http://10.73.10.19:8700 \
+ --spring.cloud.config.label=b39a0635
